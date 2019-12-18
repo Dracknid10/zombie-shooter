436e850dde61697866d64734146edf805d1c8b9b
Dracknid10 <55700825+Dracknid10@users.noreply.github.com>
2019-12-18 18:03:41 +0000

    current project is the most current version to open from

---
+++ james' zombieshooter/zombieshooter/Assets/scripts/MouseSmoothingLook2D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSmoothingLook2D : MonoBehaviour
+{
+
+    public Camera thecamera;
+    public float smoothing = 5.0f;
+    public float adjustmentAngle = 0.0f;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+
+        Vector3 target = thecamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 difference = target - transform.position;
+
+        difference.Normalize();
+
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
+
+
+
+
+    }
+
+
+}
