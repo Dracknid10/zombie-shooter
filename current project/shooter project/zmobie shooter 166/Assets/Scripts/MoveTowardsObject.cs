9bcc08b69760eeff4211e478b2e404556dc07e3c
Dracknid10 <55700825+Dracknid10@users.noreply.github.com>
2019-12-21 18:46:30 +0000

    tried to get health and score working

--- current project/shooter project/zmobie shooter 166/Assets/Scripts/MoveTowardsObject.cs
+++ current project/shooter project/zmobie shooter 166/Assets/Scripts/MoveTowardsObject.cs
@@ -11,6 +11,11 @@ public class MoveTowardsObject : MonoBehaviour
     public float speed = 5.0f;
 
 
+    public float smoothing = 5.0f;
+    public float adjustmentAngle = 0.0f;
+
+
+
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("zombiesPolice").transform;
@@ -21,13 +26,35 @@ public class MoveTowardsObject : MonoBehaviour
     {
         if (target != null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f);
-        }
-        else
-        {
 
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f);
+            Vector3 difference = target.position - transform.position;
+            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            Quaternion newRot = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRot, Time.deltaTime * smoothing);
+
+        } else
+        {
             target = GameObject.FindGameObjectWithTag("Zombie").transform;
+
         }
+
+
+
+        //else
+        //{
+
+        //    target = GameObject.FindGameObjectWithTag("Zombie").transform;
+        //}
+        //else
+        //{
+        //    target = GameObject.FindGameObjectWithTag("player").transform;
+        //}
+
+        
+       
+
+        
       
 
 
